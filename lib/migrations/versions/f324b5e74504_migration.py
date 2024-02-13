7b4eab3c2b5cace7d9c5901cc9a493c003331d6a
John-thabuks <thabuksjohn@gmail.com>
2024-02-13 19:45:45 +0300

    Done

---
+++ lib/migrations/versions/f324b5e74504_migration.py
@@ -0,0 +1,24 @@
+"""Migration
+
+Revision ID: f324b5e74504
+Revises: 3cf5ba457650
+Create Date: 2024-02-13 18:57:00.389437
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f324b5e74504'
+down_revision = '3cf5ba457650'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
